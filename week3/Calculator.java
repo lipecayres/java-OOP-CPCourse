9d4d525d18d2cb6a8330a53fe06d12e0314d2d51
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-05-29 17:56:33 -0400

    calculator class ok

--- week3/Calculator.java
+++ week3/Calculator.java
@@ -1,39 +1,36 @@
-public class Calculator{
+public class Calculator {
 
-    public static double performCalculation(int num1, int num2, String operation) {
-  
-      double math = 0;
-  
-      if (operation == "+") {
-        math = num1+num2;
-      } else if (operation == "-") {
-        math = num1-num2;
-      } else if (operation == "avg") {
-        math = (num1+num2)/2;
-      } else {
-        System.out.println("Enter a valid operation");
-      }
-      return math;
+  public static double performCalculation(double num1, double num2, String operation) {
+
+    double math = 0;
+
+    if (operation.equals("+")) {
+      math = num1 + num2;
+    } else if (operation.equals("-")) {
+      math = num1 - num2;
+    } else if (operation.equalsIgnoreCase("avg")) {
+      math = (num1 + num2) / 2;
+    } else {
+      System.out.println("Enter a valid operation");
     }
-  
-    
-    public static void performCalculation2(int num1, int num2, String operation) {
-  
-      double math = 0;
-      switch(operation){
-              case "+":
-                math = num1+num2;
-                System.out.println(math);
-              case "-":
-                math = num1-num2;
-                System.out.println(math);
-              case "avg":
-                math = (num1+num2)/2;
-                System.out.println(math);
-              default:
-                System.out.println("Enter a valid operation");
-              }
-    }
+    return math;
   }
-  
-  
+
+  public static void performCalculation2(double num1, double num2, String operation) {
+
+    double math = 0;
+    switch (operation) {
+      case "+":
+        math = num1 + num2;
+        break;
+      case "-":
+        math = num1 - num2;
+        break;
+      case "avg":
+        math = (num1 + num2) / 2;
+        break;
+    }
+    System.out.println(math);
+
+  }
+}
