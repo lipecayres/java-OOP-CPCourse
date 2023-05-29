2733fdda4eaaa171e278103c10c7f9820e6f5dac
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-05-29 17:56:49 -0400

    phone class ok

--- week3/Phone.java
+++ week3/Phone.java
@@ -1,31 +1,41 @@
-public class Phone{
+public class Phone {
     String IMEI, brand, model;
-  
-    public Phone (String IMEI, String brand, String model) {
-          this.IMEI = IMEI;
-          this.brand = brand;
-          this.model = model;
+    public static int emergency = 911;
+
+    public Phone(String IMEI, String brand, String model) {
+        this.IMEI = IMEI;
+        this.brand = brand;
+        this.model = model;
     }
-  
+
     public void setIMEI(String IMEI) {
         this.IMEI = IMEI;
     }
-    public String getIMEI(){
+
+    public String getIMEI() {
         return IMEI;
     }
-  
+
     public void setBrand(String brand) {
         this.brand = brand;
     }
-    public String getBrand(){
+
+    public String getBrand() {
         return brand;
     }
-  
+
     public void setModel(String model) {
         this.model = model;
     }
-    public String getModel(){
+
+    public String getModel() {
         return model;
     }
-    
-  }
+
+    // Step 8 and 9 | Emergency method
+    public void callEmergency() {
+        System.out.printf("Dialing %s", Phone.emergency);
+
+    }
+
+}
