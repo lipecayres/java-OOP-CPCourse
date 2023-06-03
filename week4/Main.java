64d9afaab0b24c56b9f9dce3d57d7d7a1867ced5
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-06-03 09:20:30 -0400

    first commit

---
+++ week4/Main.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class Main {
+ public static void main(String[] args) {
+
+   Scanner sc = new Scanner(System.in);
+   
+   // array of names
+   String[];
+
+   // variable to get names
+   String names;
+   int randomNumber;
+
+   System.out.println("Lucky prize... Let's start!");
+
+   while(.names.equals("quit")){
+     
+     System.out.println("Lucky prize... Let's start!");    
+     names = sc.nextLine();
+
+
+     
+   }
+ }
+}
