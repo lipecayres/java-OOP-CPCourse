eafb78fafe90f8d4f880c1bf81554367cdff4028
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-06-03 09:20:23 -0400

    first commit

---
+++ week4/Animal.java
@@ -0,0 +1,28 @@
+public class Animal{
+    private String name;
+    private double topSpeed;
+   
+     // default constructor
+     public Animal () {};
+   
+     public Animal (String name, double topSpeed) {
+       this.name = name;
+       this.topSpeed = topSpeed;
+     }
+   
+     // getters and setters 
+     public void setName(String name) {
+         this.name = name;
+     }
+     public String getName(){
+         return name;
+     }
+   
+     public void setTopSpeed(double topSpeed) {
+         this.topSpeed = topSpeed;
+     }
+     public double getTopSpeed(){
+         return topSpeed;
+     }
+     
+   }
