a0b38134ff0673e3f59925f9e8a8ec18f63291dc
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-06-13 16:11:47 -0400

    first commit main

---
+++ week6/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+  ArrayList<Students> students = new ArrayList<>();
+
+  Student student1 = new Student()
+
+
+
+    
+  }
+}
