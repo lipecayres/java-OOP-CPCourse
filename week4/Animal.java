7dd64f8433d5d828f3d4e5b68db452ad73a39145
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-06-13 13:27:21 -0400

    animal ok

--- week4/Animal.java
+++ week4/Animal.java
@@ -1,28 +1,31 @@
-public class Animal{
+public class Animal {
     private String name;
     private double topSpeed;
-   
-     // default constructor
-     public Animal () {};
-   
-     public Animal (String name, double topSpeed) {
-       this.name = name;
-       this.topSpeed = topSpeed;
-     }
-   
-     // getters and setters 
-     public void setName(String name) {
-         this.name = name;
-     }
-     public String getName(){
-         return name;
-     }
-   
-     public void setTopSpeed(double topSpeed) {
-         this.topSpeed = topSpeed;
-     }
-     public double getTopSpeed(){
-         return topSpeed;
-     }
-     
-   }
+
+    // default constructor
+    public Animal() {
+    };
+
+    public Animal(String name, double topSpeed) {
+        this.name = name;
+        this.topSpeed = topSpeed;
+    }
+
+    // getters and setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setTopSpeed(double topSpeed) {
+        this.topSpeed = topSpeed;
+    }
+
+    public double getTopSpeed() {
+        return topSpeed;
+    }
+
+}
